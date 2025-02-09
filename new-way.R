e1beb0fcdf8a65ca1c5ffc9701cc2fa5487d659d
asenetcky <asenetcky@gmail.com>
2025-02-09 13:22:43 -0500

    basic overview

---
+++ new-way.R
@@ -0,0 +1,12 @@
+# Using renv
+
+# So the easiest way to use renv is to create a new project
+# and then click the "use renv" click box in the dialog.
+
+# Workflow
+
+# install with renv
+# use the packages
+# snapshot
+# share
+# renv restore
