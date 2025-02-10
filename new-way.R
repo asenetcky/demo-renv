dda00c332122b4b2f16e75e581043ab071c7763d
asenetcky <asenetcky@gmail.com>
2025-02-10 07:23:33 -0500

    showcase renv cache

--- new-way.R
+++ new-way.R
@@ -5,8 +5,43 @@
 
 # Workflow
 
+# nice balance between organic and intentional
+
 # install with renv
 # use the packages
 # snapshot
 # share
 # renv restore
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# showcasing the cache
+renv::install("purrr")
+
+rep("purrr", 10) |>
+  purrr::walk(
+    \(pkg){
+      renv::install(pkg, prompt = FALSE)
+    }
+  )
+
+
+# it's not going to update the package on you
+# unless you specifically tell it to.
+
+renv::install("purrr@1.0.0", prompt = FALSE)
+
+renv::update("purrr")
